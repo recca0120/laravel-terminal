b6d2a515893383bf11708258f4510577c3c4773b
unknown <recca0120@gmail.com>
2015-11-17 10:10:25 +0800

    artisan change stream response to response

--- src/Http/Controllers/TerminalController.php
+++ src/Http/Controllers/TerminalController.php
@@ -2,7 +2,6 @@
 
 namespace Recca0120\Terminal\Http\Controllers;
 
-use Application;
 use Artisan;
 use Exception;
 use Illuminate\Http\Request;
@@ -66,49 +65,44 @@ class TerminalController extends Controller
 
     public function artisan(Request $request)
     {
-        return response()->stream(function () use ($request) {
+        // set_time_limit(30);
+        $result = null;
+        $error = null;
 
-            // set_time_limit(30);
-            $result = null;
-            $error = null;
+        $id = $request->input('id');
+        $command = $request->input('method');
 
-            $id = $request->input('id');
-            $command = $request->input('method');
-
-            $temp = array_map(function ($item) {
-                if (starts_with($item, '--') && strpos($item, '=') === false) {
-                    $item .= '=default';
-                }
-
-                return $item;
-            }, $request->input('params', []));
-            $parameters = [];
-            foreach ($temp as $tmp) {
-                $explodeTmp = explode('=', $tmp);
-                $parameters[array_get($explodeTmp, 0)] = array_get($explodeTmp, 1, '');
+        $temp = array_map(function ($item) {
+            if (starts_with($item, '--') && strpos($item, '=') === false) {
+                $item .= '=default';
             }
 
-            try {
-                $parameters['command'] = $command;
-                $lastOutput = new BufferedOutput(BufferedOutput::VERBOSITY_NORMAL, true, $this->outputFormatter);
-                $exitCode = Artisan::handle(new ArrayInput($parameters), $lastOutput);
-                $result = $lastOutput->fetch();
-                // exit;
-                // $exitCode = Artisan::call($command, $parameters);
-                // $result = Artisan::output();
-            } catch (Exception $e) {
-                $result = false;
-                $error = $e->getMessage();
-            }
+            return $item;
+        }, $request->input('params', []));
+        $parameters = [];
+        foreach ($temp as $tmp) {
+            $explodeTmp = explode('=', $tmp);
+            $parameters[array_get($explodeTmp, 0)] = array_get($explodeTmp, 1, '');
+        }
 
-            echo json_encode([
-                'jsonrpc' => '2.0',
-                'result' => $result,
-                'id' => $id,
-                'error' => $error,
-            ]);
-        }, 200, [
-            'content-type' => 'application/json',
+        try {
+            $parameters['command'] = $command;
+            $lastOutput = new BufferedOutput(BufferedOutput::VERBOSITY_NORMAL, true, $this->outputFormatter);
+            $exitCode = Artisan::handle(new ArrayInput($parameters), $lastOutput);
+            $result = $lastOutput->fetch();
+            // exit;
+            // $exitCode = Artisan::call($command, $parameters);
+            // $result = Artisan::output();
+        } catch (Exception $e) {
+            $result = false;
+            $error = $e->getMessage();
+        }
+
+        return response()->json([
+            'jsonrpc' => '2.0',
+            'result' => $result,
+            'id' => $id,
+            'error' => $error,
         ]);
     }
 }
