e857ef87f753564eaf1e4477543c9c3a101e7024
Recca Tsai <recca0120.gmail.com>
2016-09-23 21:18:12 +0800

    support laravel 5.2 up

--- src/Application.php
+++ src/Application.php
@@ -154,22 +154,9 @@ class Application extends ConsoleApplication
     {
         // Illuminate\Console\Events\ArtisanStarting
         if (is_null($this->laravel['events']) === false) {
-            return $this->laravel['events']->firing() === $this->getArtisanString();
+            return $this->laravel['events']->firing() === ArtisanStarting::class;
         }
 
         return false;
-    }
-
-    /**
-     * getArtisanString.
-     *
-     * @method getArtisanString
-     *
-     * @return string
-     */
-    private function getArtisanString()
-    {
-        return (version_compare($this->laravel->version(), 5.2, '>=') === true) ?
-            ArtisanStarting::class : 'artisan.start';
     }
 }
