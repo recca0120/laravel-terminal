ca5bd1d0e788be05a22c33f6f2d89626f107fb62
recca0120 <recca0120@gamil.com>
2018-08-05 00:30:29 +0800

    update

--- resources/assets/ts/app.d.ts
+++ resources/assets/ts/app.d.ts
@@ -2,10 +2,14 @@ import 'jquery-mousewheel';
 import 'jquery.terminal';
 import './unix_formatting';
 export declare class Terminal {
+    private options;
     private element;
+    private outputFormatter;
     private commands;
     private term;
     constructor(elementId: any, options: any);
     run(cmd: string): Promise<void>;
-    fit(): void;
+    private prompt;
+    private greetings;
+    private fit;
 }
--- resources/assets/ts/app.ts
+++ resources/assets/ts/app.ts
@@ -3,7 +3,8 @@ import 'jquery-mousewheel';
 import 'jquery.terminal';
 import './unix_formatting';
 import { HttpClient } from './httpclient';
-import { Artisan, Composer, MySQL, Tinker, Vim } from './commands';
+import { Artisan, Composer, MySQL, Tinker, Vim, Help } from './commands';
+import { OutputFormatter } from './output-formatter';
 
 const win: any = <any>window;
 $.terminal.defaults.unixFormattingEscapeBrackets = true;
@@ -11,24 +12,31 @@ $.terminal['ansi_colors']['normal'] = $.terminal['ansi_colors']['bold'];
 
 export class Terminal {
     private element: HTMLDivElement;
+    private outputFormatter = new OutputFormatter();
     private commands = [];
     private term;
 
-    constructor(elementId, options: any) {
-        const client = new HttpClient(options.endpoint, {
-            'X-CSRF-TOKEN': options.csrfToken,
+    constructor(elementId, private options: any) {
+        const client = new HttpClient(this.options.endpoint, {
+            'X-CSRF-TOKEN': this.options.csrfToken,
         });
 
         this.commands = [
-            new Artisan(client),
-            new Composer(client),
-            new MySQL(client),
-            new Tinker(client),
-            new Vim(client),
+            new Artisan(client, this.options),
+            new Composer(client, this.options),
+            new Help(client, this.options),
+            new MySQL(client, this.options),
+            new Tinker(client, this.options),
+            new Vim(client, this.options),
         ];
 
         this.element = document.querySelector(elementId);
-        this.term = $(this.element).terminal(this.run.bind(this), {});
+        this.term = $(this.element).terminal(this.run.bind(this), {
+            greetings: this.greetings(),
+            prompt: this.prompt(),
+        } as any);
+
+        this.run('help');
 
         this.fit();
         window.addEventListener('resize', this.fit.bind(this));
@@ -50,7 +58,40 @@ export class Terminal {
         }
     }
 
-    fit() {
+    private prompt() {
+        // const host = [this.options.username, '@', this.options.hostname].reduce(
+        //     (prev, next) => prev + this.outputFormatter.info(next),
+        //     ''
+        // );
+        // const os = this.outputFormatter.question(this.options.os);
+        // const path = this.outputFormatter.comment(this.options.basePath);
+
+        // return `${host} ${os} ${path} $ `;
+
+        const host = [this.options.username, '@', this.options.hostname].reduce(
+            (prev, next) => prev + this.outputFormatter.info(next),
+            ''
+        );
+        const path = this.outputFormatter.comment('~');
+
+        return `${host}:${path} $ `;
+    }
+
+    private greetings() {
+        return `
+ __                        _    _____              _         _
+|  |   ___ ___ ___ _ _ ___| |  |_   ____ ___ _____|_|___ ___| |
+|  |__| .'|  _| .'| | | -_| |    | || -_|  _|     | |   | .'| |
+|_____|__,|_| |__,|\\_/|___|_|    |_||___|_| |_|_|_|_|_|_|__,|_|
+
+Copyright (c) 2018 Recca Tsai <https://github.com/recca0120/>
+
+Type a command, or type \`${this.outputFormatter.info('help')}\`, for a list of commands.
+
+`;
+    }
+
+    private fit(): void {
         const parent: HTMLDivElement = this.element.parentNode as HTMLDivElement;
         if (parent.tagName === 'BODY') {
             this.element.style.width = `${win.innerWidth}px`;
