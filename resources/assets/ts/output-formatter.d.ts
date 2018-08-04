5562aa82abfcbeb60695b7839bd8df06a1a08066
recca0120 <recca0120@gamil.com>
2018-08-05 00:30:21 +0800

    output format

---
+++ resources/assets/ts/output-formatter.d.ts
@@ -0,0 +1,25 @@
+import 'jquery.terminal';
+import './unix_formatting';
+export declare class OutputFormatterStyle {
+    private colors;
+    private foreground;
+    private background;
+    constructor(foreground?: string, background?: string);
+    apply(text: any): string;
+    private getColor;
+}
+export declare class OutputFormatter {
+    private formatters;
+    error(text: any): any;
+    info(text: any): any;
+    comment(text: any): any;
+    question(text: any): any;
+    black(text: any): any;
+    red(text: any): any;
+    green(text: any): any;
+    yellow(text: any): any;
+    blue(text: any): any;
+    magenta(text: any): any;
+    cyan(text: any): any;
+    white(text: any): any;
+}
