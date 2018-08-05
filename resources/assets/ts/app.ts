2e63c1dd895bc950a9b2d32c071dcd3bcea0840e
recca0120 <recca0120@gamil.com>
2018-08-05 17:39:41 +0800

    update

--- resources/assets/ts/app.ts
+++ resources/assets/ts/app.ts
@@ -150,7 +150,7 @@ export class Terminal {
                     }
                 },
                 {
-                    prompt: '> ',
+                    prompt: ' > ',
                 }
             );
         });
